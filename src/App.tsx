6ccaffdf51f970613681e1780ccdea79ea5286e7
KelwinGerson <83376362+KelwinGerson@users.noreply.github.com>
2022-11-11 08:39:13 -0300

    feat: styled components theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { ThemeProvider } from 'styled-components'
 import { Button } from "./components/Button";
+import { defaultTheme } from './components/styles/themes/default';
 
 export function App() {
   return (
-    <>
-    <Button variant="primary"/>
-    <Button variant="danger"/>
-    <Button variant="secondary"/>
-    <Button/>
-    </>
+    //import theme default
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="danger"/>
+      <Button variant="secondary"/>
+      <Button/>
+    </ThemeProvider>
   )
 }
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -18,11 +18,17 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 
     width: 100px;
     height: 40px;
-    
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
+
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.black};
+
     //interpolação
-    ${props => {
+    /* ${props => {
         return css`
         background-color: ${buttonVariants[props.variant]}
         `
-    }}
+    }} */
 `
